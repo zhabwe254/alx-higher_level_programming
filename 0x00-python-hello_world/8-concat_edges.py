e2d466027295b66b648fa3f2ed021ba7919b414c
zhabwe254 <zhabwe@gmail.com>
2023-11-28 02:35:11 +0300

    a

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# This Python script prints a new sentence
 
-str = "object-oriented programming with Python"
-
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
