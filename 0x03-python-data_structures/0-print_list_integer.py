297f8abf9342a01b2465f0ca017e006bcdb0ffc0
zhabwe254 <zhabwe@gmail.com>
2023-12-03 15:52:02 +0300

    A

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,3 @@
+def print_list_integer(my_list=[]):
+    for number in my_list:
+        print("{:d}".format(number))
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx >= 0 and idx < len(new_list):
+        new_list[idx] = element
+    return new_list
+
+# Example usage
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
