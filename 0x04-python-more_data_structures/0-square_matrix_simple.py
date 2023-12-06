c0d376b9832917aa23b29e2004445ec18850ccd0
zhabwe254 <zhabwe@gmail.com>
2023-12-07 01:55:08 +0300

    a

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-	"""
-	Squares all elements in a matrix.
-
-	Args:
-		matrix: A 2D list of integers.
-
-	Returns:
-		A new 2D list with squared elements.
-	"""
-	return [[element ** 2 for element in row] for row in matrix]
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix
