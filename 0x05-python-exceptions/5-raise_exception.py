d45266f57b65de37aa29e1206430c833861fb9e5
zhabwe254 <zhabwe@gmail.com>
2023-12-18 22:22:18 +0300

    a

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def raise_exception():
-    raise TypeError("An exception is raised")
-
-if __name__ == "__main__":
-    try:
-        raise_exception()
-    except TypeError as te:
-        print("Exception raised")
+def raise_exception_msg(message=""):
+    raise NameError(message)
