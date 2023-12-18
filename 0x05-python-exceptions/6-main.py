25937010d921ea9439647f57703b684b6a2bdaab
zhabwe254 <zhabwe@gmail.com>
2023-12-18 22:26:31 +0300

    a

--- 0x05-python-exceptions/6-main.py
+++ 0x05-python-exceptions/6-main.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def raise_exception_msg(message=""):
-    raise NameError(message)
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
