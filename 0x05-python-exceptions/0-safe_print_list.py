4c2548432fc48a2b2619f4c63508047e105d99ca
zhabwe254 <zhabwe@gmail.com>
2023-12-18 20:20:50 +0300

    a

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 		count = 0
 		for i in range(x):
 			print(my_list[i], end="")
-		count += 1
+			count += 1
 		print()
 		return count
 	except IndexError:
@@ -13,10 +13,10 @@ def safe_print_list(my_list=[], x=0):
 		return count
 
 if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    nb_print = safe_print_list(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
+	my_list = [1, 2, 3, 4, 5]
+	nb_print = safe_print_list(my_list, 2)
+	print("nb_print: {:d}".format(nb_print))
+	nb_print = safe_print_list(my_list, len(my_list))
+	print("nb_print: {:d}".format(nb_print))
+	nb_print = safe_print_list(my_list, len(my_list) + 2)
+	print("nb_print: {:d}".format(nb_print))
