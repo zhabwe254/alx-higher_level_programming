09017ea0135917bc922549e4e44a36d67c7bba8a
zhabwe254 <zhabwe@gmail.com>
2023-12-18 22:33:23 +0300

    a

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
-	result_list = []
-	for i in range(list_length):
-	try:
-	result = my_list_1[i] / my_list_2[i]
-	except (ZeroDivisionError, TypeError):
-		print("wrong type")
-		result = 0
-	except IndexError:
-		print("out of range")
-		result = 0
-	finally:
-		result_list.append(result)
-	return result_list
-
-if __name__ == "__main__":
-	my_l_1 = [10, 8, 4]
-	my_l_2 = [2, 4, 4]
-	result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-	print(result)
-
-	print("--")
-
-	my_l_1 = [10, 8, 4, 4]
-	my_l_2 = [2, 0, "H", 2, 7]
-	result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-	print(result)
+    n_list = []
+    for i in range(list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            n_list.append(div)
+    return n_list
