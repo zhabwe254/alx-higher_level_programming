1b8d2d35e895ff04ec87f702ff92d15d9f515a11
zhabwe254 <zhabwe@gmail.com>
2024-01-05 11:02:32 +0300

    a

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Rectangle:
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if
