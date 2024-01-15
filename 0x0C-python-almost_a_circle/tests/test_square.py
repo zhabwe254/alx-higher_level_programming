ce0d09e99525c66724934940f70fbb6f741d2cbb
zhabwe254 <zhabwe@gmail.com>
2024-01-16 02:57:41 +0300

    A

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -1,3 +1,4 @@
+94% of storage used … If you run out, you won't have enough storage to create, edit and upload files. Get 100 GB of storage for Ksh 250.00 Ksh 60.00/month for 3 months.
 #!/usr/bin/python3
 # test_square.py
 # Brennan D Baraban <375@holbertonschool.com>
