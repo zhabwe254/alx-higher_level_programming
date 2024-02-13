c3802f32e91b570d5fccc8074eb98b76e6f19689
zhabwe254 <zhabwe@gmail.com>
2024-02-13 09:42:02 -0800

    A

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,3 @@
-Write a script that lists all the tables of a database in your MySQL server.
+--Write a script that lists all the tables of a database in your MySQL server.
+USE mysql;
 SHOW TABLES;
