05ee7fbc2ff7ff831401e21f53758d32e38d2e0d
zhabwe254 <zhabwe@gmail.com>
2024-02-13 09:12:29 -0800

    a

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
--- The database name will be passed as an argument of the mysql command.
+-- Prints the full description of the table first_table in my MySQL server.
+SHOW CREATE TABLE `first_table`;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+DESCRIBE first_table;
+
+-- 6. List all in table
+-- Write a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command
