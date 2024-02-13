fb0d80152bce89ef259631f63dda7ad385ec828e
zhabwe254 <zhabwe@gmail.com>
2024-02-13 09:35:01 -0800

    a

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,5 @@
 -- 2. Delete a database
 -- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail.
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -5,4 +5,6 @@
 -- id INT
 -- name VARCHAR(256)
 -- The database name will be passed as an argument of the mysql command
--- If the table first_table already exists, your script should not fail
+-- If the table first_table already exists, your script should not fail.
+
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
