9e2f1cae7a047772a3b91a5accf6ac0625bfec52
zhabwe254 <zhabwe@gmail.com>
2024-03-11 21:47:08 +0300

    a

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length <= 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(args[2]);
+  console.log(process.argv[2]);
 }
-
