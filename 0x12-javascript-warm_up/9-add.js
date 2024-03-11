c86b8403be28a4676d22a5c23103f709d33e9c41
zhabwe254 <zhabwe@gmail.com>
2024-03-11 22:02:19 +0300

    a

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
-function add (a, b) {
-  const c = a + b;
-  console.log(c);
+function add(a, b) {
+  return a + b;
 }
 
-add(Number(process.argv[2]), Number(process.argv[3]));
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  console.log(add(num1, num2));
+}
+module.exports = { add };
