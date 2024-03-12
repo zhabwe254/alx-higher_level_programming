0f1caa98da90928e77f85e6ab4040c2ac7ba2710
zhabwe254 <zhabwe@gmail.com>
2024-03-12 23:10:19 +0300

    a

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) throw err;
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) throw err;
+    const concatenatedData = dataA.trim() + '\n' + dataB.trim();
+    fs.writeFile(fileC, concatenatedData, err => {
+      if (err) throw err;
+      console.log('The files have been concatenated!');
+    });
+  });
+});
