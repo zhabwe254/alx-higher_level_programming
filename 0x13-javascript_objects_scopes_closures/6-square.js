a8ae39ad86647e5d63e0e421980fb66328c864de
zhabwe254 <zhabwe@gmail.com>
2024-03-12 22:30:05 +0300

    A

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class SquareCharPrint extends Square {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = SquareCharPrint;
