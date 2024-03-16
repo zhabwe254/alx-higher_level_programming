4796126b3a985ad1d1bf9e5b7d38992ebcc3824a
zhabwe254 <zhabwe@gmail.com>
2024-03-16 21:11:04 +0300

    a

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""
-Script that takes a state name as an argument and lists all cities of that state.
-"""
+"""Lists all cities from the database hbtn_0e_0_usa"""
 
-import sys
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(sys.argv[0]))
-        exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database)
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id", (state_name,))
-    cities = cursor.fetchall()
-    city_names = [city[0] for city in cities]
-    print(", ".join(city_names))
-
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
     db.close()
