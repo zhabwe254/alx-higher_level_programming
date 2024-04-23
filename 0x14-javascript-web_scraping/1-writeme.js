9b1b2e90a233a2b2f7b34ef10ab53ac7daebad77
zhabwe254 <zhabwe@gmail.com>
2024-04-23 17:48:04 +0300

    a

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log('The file has been saved!');
+  console.log(`The file ${filePath} has been saved!`);
 });
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
+  fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
     if (err) {
       console.error(err);
       return;
